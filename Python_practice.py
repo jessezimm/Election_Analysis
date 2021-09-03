3105ad6f9eaac754a887c1e4aa2a455bf99c4444
jessezimm <jessezimm@gmail.com>
2021-09-03 07:51:10 -0400

    updating PyPoll.py file

---
+++ Python_practice.py
@@ -0,0 +1,8 @@
+print("Hello World")
+type(3)
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+        print(counties[1])
+if counties[3] != 'Jefferson'
+        print(counties[2])
+
